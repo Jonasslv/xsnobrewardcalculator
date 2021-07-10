4b96c4dfbdc27a08b038170d6067e69392d64a7c
Jonasslv <jowsba@hotmail.com>
2021-07-10 15:44:21 -0300

    Added JOE Revenue

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const fs = require('fs');
 const readline = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout
-  });
+});
 
 const settings = JSON.parse(fs.readFileSync('./settings.json'));
 const listStrategyContracts = JSON.parse(fs.readFileSync('./strategycontracts.json'));
-const { retrievePNGPrice } = require('./src/graph.js'); 
+const { retrievePNGPrice, retrieveJOEPrice } = require('./src/graph.js');
 const { Constants } = require('./src/resources.js');
 
-async function genericQuery(pangolinAddress,limit,skip,key) {
+async function genericQuery(contractAddress, limit, skip, key) {
     let query = await axios({
-        url: `${Constants.covalentAPIURL}${pangolinAddress}/transactions_v2/?limit=${limit}&key=${key}&skip=${skip}&page-size=15000`,
+        url: `${Constants.covalentAPIURL}${contractAddress}/transactions_v2/?limit=${limit}&key=${key}&skip=${skip}&page-size=12000`,
         method: 'get'
     }).catch(error => {
         console.error(error)
@@ -21,8 +21,8 @@ async function genericQuery(pangolinAddress,limit,skip,key) {
     return query;
 }
 
-function registerDate(date){
-    if(Date.parse(date) == NaN){
+function registerDate(date) {
+    if (Date.parse(date) == NaN) {
         console.log('Invalid Date!');
         process.exit(1); //invalid date
     }
@@ -35,25 +35,25 @@ var endingDate;
 
 
 console.log(Constants.snowLogo);
-console.log('See the PNG harvested in all Snowglobes between 2 dates (UTC)!');
+console.log('See the Tokens harvested in all Snowglobes between 2 dates (UTC)!');
 console.log('Beware that higher timeframes can take a lot of time to process.');
 readStartingDate();
 
 
-function readStartingDate(){
+function readStartingDate() {
     readline.question('Input the Starter Date (yyyy-MM-dd):', date => {
         starterDate = registerDate(date);
         readEndingDate();
-   });
+    });
 }
 
-function readEndingDate(){
+function readEndingDate() {
     readline.question('Input the Ending Date (yyyy-MM-dd):', date => {
         endingDate = registerDate(date);
         readline.close();
-        if(starterDate > endingDate){
-            console.log('Invalid date setup.'); 
-            process.exit(1);   
+        if (starterDate > endingDate) {
+            console.log('Invalid date setup.');
+            process.exit(1);
         }
         console.log('Queries are being performed... Wait a Little.');
         searchTransactions();
@@ -61,68 +61,93 @@ function readEndingDate(){
     });
 }
 
-async function searchTransactions(){
+async function searchTransactions() {
     let jsonObject = new Object;
-    if(settings.saveToJSON){
+    if (settings.saveToJSON) {
         jsonObject.contractList = [];
         jsonObject.starterDate = starterDate;
         jsonObject.endingDate = endingDate;
     }
 
     //internal function that does the covalent API query
-    async function mountTransactionList(element,transactionList,skip){
-        let queryResult = (await genericQuery(element.pangolinpool,Constants.standardLimit,skip,settings.covalentAPIKey)).data.data.items;
+    async function mountTransactionList(element, transactionList, skip) {
+        let queryResult = (await genericQuery(element.stakingAddress, Constants.standardLimit, skip, settings.covalentAPIKey)).data.data.items;
         transactionList = transactionList.concat(queryResult);
-        let existsDate = lodash.filter(transactionList,function(o) { 
+        let existsDate = lodash.filter(transactionList, function (o) {
             let blockDate = new Date(o.block_signed_at);
             let onlyDate = new Date(blockDate.toDateString());
             return starterDate >= onlyDate;
         });
         //dates exists or the address don't have more transactions
-        if(existsDate.length > 0 || queryResult.length < 1000){
-            return {result:true, list:transactionList};
-        }else{
+        if (existsDate.length > 0 || queryResult.length < 1000) {
+            return { result: true, list: transactionList };
+        } else {
             console.log('Still Processing...');
             //recursive til find the date
             skip += Constants.standardSkip;
-            return {result:false, list:transactionList, skip: skip};
+            return { result: false, list: transactionList, skip: skip };
         }
     }
 
-    let totalPNGHarvested = 0;
-    let lenList = listStrategyContracts.length;
-    let counter = 0;
+    //cache to masterchefJoe transactions so we don`t need to query it again, just filter
+    var masterchefJoeTransactions = [];
+    var totalPNGHarvested = 0;
+    var totalJOEHarvested = 0;
+    var lenList = listStrategyContracts.length;
+    var counter = 0;
     console.log(`Doing Calculations between ${starterDate} and ${endingDate}`);
     //for every snowglobe x staking pangolin pool
-    do{
+    do {
         let transactionsList = [];
         let listValidTransactions = [];
-        var skip = 0;
-        foundDate = false;
-        //search all transactions loosely between 2 dates (with skip/limit)
-        while(!foundDate){
-            result = await mountTransactionList(listStrategyContracts[counter],transactionsList,skip);
-            foundDate = result.result;
-            skip = result.skip;
-            transactionsList = result.list;
+        let poolTransactions = listStrategyContracts[counter].protocol == 'Pangolin'; 
+        if(listStrategyContracts[counter].protocol == 'Trader Joe' && masterchefJoeTransactions.length == 0){
+            poolTransactions = true;
+        }
+        if (poolTransactions) {
+            var skip = 0;
+            foundDate = false;
+            //search all transactions loosely between 2 dates (with skip/limit)
+            while (!foundDate) {
+                result = await mountTransactionList(listStrategyContracts[counter], transactionsList, skip);
+                foundDate = result.result;
+                skip = result.skip;
+                transactionsList = result.list;
+            }
+
+            //enforces the 2 dates
+            listValidTransactions = lodash.filter(transactionsList, function (o) {
+                let blockDate = new Date(o.block_signed_at);
+                let onlyDate = new Date(blockDate.toDateString());
+                return ((starterDate <= onlyDate) && (endingDate >= onlyDate));
+            });
         }
 
-        //enforces the 2 dates
-        listValidTransactions = lodash.filter(transactionsList,function(o) { 
-            let blockDate = new Date(o.block_signed_at);
-            let onlyDate = new Date(blockDate.toDateString());
-            return ((starterDate <= onlyDate) && (endingDate >= onlyDate));
-        });
+        if (listStrategyContracts[counter].protocol == 'Trader Joe') {
+            if(masterchefJoeTransactions.length == 0){
+                masterchefJoeTransactions = listValidTransactions;
+            }else{
+                listValidTransactions = masterchefJoeTransactions;
+            }
+        }
 
         //get only the internal transactions
         let allDecodedEvents = [];
-        listValidTransactions.forEach((element)=>{
+        listValidTransactions.forEach((element) => {
             //only successful transactions
-            if(element.successful = true){
+            if (element.successful) {
                 element.log_events.forEach((element2) => {
-                    //only PNG Transactions
-                    if(element2.sender_address.toLowerCase() == Constants.PNGContract.toLowerCase()){
-                        if(element2.decoded){
+                    //only token transactions
+                    let tokenAddress;
+                    switch (listStrategyContracts[counter].protocol) {
+                        case 'Pangolin':
+                            tokenAddress = Constants.PNGContract;
+                            break;
+                        case 'Trader Joe':
+                            tokenAddress = Constants.JoeContract;
+                    }
+                    if (element2.sender_address.toLowerCase() == tokenAddress.toLowerCase()) {
+                        if (element2.decoded) {
                             allDecodedEvents = allDecodedEvents.concat(element2.decoded);
                         }
                     }
@@ -130,9 +155,9 @@ async function searchTransactions(){
             }
         });
         listValidTransactions = [];
-        
+
         //get only the transfer transactions
-        let onlyTransfers = lodash.filter(allDecodedEvents,function(o) { 
+        let onlyTransfers = lodash.filter(allDecodedEvents, function (o) {
             return o.name.startsWith('Transfer');
         });
         allDecodedEvents = [];
@@ -144,35 +169,45 @@ async function searchTransactions(){
             let validTo = false;
             let validFrom = false;
             element.params.forEach((element2) => {
-                if(validTo && validFrom){
-                    if(element2.name == 'value'){
-                        totalPNGHarvested += (element2.value*1);
-                        contractHarvested += (element2.value*1);
-                    } 
+                if (validTo && validFrom) {
+                    if (element2.name == 'value') {
+                        switch (listStrategyContracts[counter].protocol) {
+                            case 'Pangolin':
+                                totalPNGHarvested += (element2.value * 1);
+                                break;
+                            case 'Trader Joe':
+                                totalJOEHarvested += (element2.value * 1);
+                        }
+                        contractHarvested += (element2.value * 1);
+                    }
                 }
-                if(element2.name == 'from'){
-                    validFrom = (element2.value.toLowerCase() == listStrategyContracts[counter].pangolinpool.toLowerCase()); 
+                if (element2.name == 'from') {
+                    validFrom = (element2.value.toLowerCase() == listStrategyContracts[counter].stakingAddress.toLowerCase());
                 }
-                if(element2.name == 'to'){
-                    validTo = (element2.value.toLowerCase() == listStrategyContracts[counter].strategy.toLowerCase()); 
+                if (element2.name == 'to') {
+                    validTo = (element2.value.toLowerCase() == listStrategyContracts[counter].strategy.toLowerCase());
                 }
             });
         });
 
-        if(settings.saveToJSON){
-            jsonObject.contractList.push({contract:listStrategyContracts[counter].strategy,name:listStrategyContracts[counter].name,harvested:contractHarvested/10 **18});
+        if (settings.saveToJSON) {
+            jsonObject.contractList.push({ contract: listStrategyContracts[counter].strategy, name: listStrategyContracts[counter].name, harvested: contractHarvested / 10 ** 18 });
         }
-        console.log(`Contract: ${listStrategyContracts[counter].strategy} (${listStrategyContracts[counter].name}) - Harvested: ${contractHarvested/ 10 ** 18} PNG`);
+        console.log(`Strategy: (${listStrategyContracts[counter].name} - ${listStrategyContracts[counter].protocol}) - Harvested: ${contractHarvested / 10 ** 18} Tokens`);
         counter++;
-    }while(counter <= lenList-1);
+    } while (counter <= lenList - 1);
 
-    if(settings.saveToJSON){
-        jsonObject.totalPNGHarvested = totalPNGHarvested/10 **18;
-        jsonObject.performanceFees = (totalPNGHarvested/10)/10 **18;
-        jsonObject.xsnobRevenue = ((totalPNGHarvested/100)*3)/ 10 **18;
-        fs.writeFileSync('./result.json',JSON.stringify(jsonObject));
+    if (settings.saveToJSON) {
+        jsonObject.totalPNGHarvested = totalPNGHarvested / 10 ** 18;
+        jsonObject.totalJOEHarvested = totalJOEHarvested / 10 ** 18;
+        jsonObject.performanceFeesPNG = (totalPNGHarvested / 10) / 10 ** 18;
+        jsonObject.xsnobRevenuePNG = ((totalPNGHarvested / 100) * 3) / 10 ** 18;
+        jsonObject.performanceFeesJOE = (totalJOEHarvested / 10) / 10 ** 18;
+        jsonObject.xsnobRevenueJOE = ((totalJOEHarvested / 100) * 3) / 10 ** 18;
+        fs.writeFileSync('./result.json', JSON.stringify(jsonObject));
     }
 
-    console.log(`Total PNG Harvested: ${totalPNGHarvested/ 10 ** 18} 10% Performance Fees: ${(totalPNGHarvested/ 10 ** 18)/10} 3% xSNOB Revenue: ${((totalPNGHarvested/ 10 ** 18)/100)*3}`);
-    console.log(`Total Value Harvested: $${(await retrievePNGPrice()*(totalPNGHarvested/ 10 ** 18))}`);
+    console.log(`Total PNG Harvested: ${totalPNGHarvested / 10 ** 18} 10% Performance Fees: ${(totalPNGHarvested / 10 ** 18) / 10} 3% xSNOB Revenue: ${((totalPNGHarvested / 10 ** 18) / 100) * 3}`);
+    console.log(`Total JOE Harvested: ${totalJOEHarvested / 10 ** 18} 10% Performance Fees: ${(totalJOEHarvested / 10 ** 18) / 10} 3% xSNOB Revenue: ${((totalJOEHarvested / 10 ** 18) / 100) * 3}`);
+    console.log(`Total Value Harvested: $${((await retrievePNGPrice() * (totalPNGHarvested / 10 ** 18)) + (await retrieveJOEPrice() * (totalJOEHarvested / 10 ** 18)))}`);
 }
--- src/resources.js
+++ src/resources.js
@@ -18,6 +18,7 @@ module.exports = {
         static USDTAVAXPairContract = "0x9ee0a4e21bd333a6bb2ab298194320b8daa26516";
         static DAIAVAXPairContract = "0x17a2e8275792b4616befb02eb9ae699aa0dcb94b";
         static PNGContract = "0x60781c2586d68229fde47564546784ab3faca982";
+        static JoeContract = "0x6e84a6216eA6dACC71eE8E6b0a5B7322EEbC0fDd";
         static covalentAPIURL = `https://api.covalenthq.com/v1/43114/address/`;
         static standardLimit = 1000;
         static standardSkip = 1000;
